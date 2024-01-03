196727efb919d8d4bc9b0b1e3bc5c4e345ea8f62
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-03 11:03:22 +0530

    commit code changes to arima

--- scripts/train_series_arima.py
+++ scripts/train_series_arima.py
@@ -34,4 +34,43 @@ plt.legend(loc='best')
 plt.title("Train and Validation Data")
 plt.show()
 
+# dickey fuller, KPSS
+from statsmodels.tsa.stattools import adfuller, kpss
+
+def adf_test(timeseries):
+    
+    #Perform Dickey-Fuller test:
+    print ('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput=pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print (dfoutput)
+
+
+adf_test(train_data['count'])
+
+def kpss_test(timeseries):
+    print ('Results of KPSS Test:')
+    kpsstest = kpss(timeseries, regression='c')
+    kpss_output = pd.Series(kpsstest[0:3], index=['Test Statistic','p-value','Lags Used'])
+    for key,value in kpsstest[3].items():
+        kpss_output['Critical Value (%s)'%key] = value
+    print (kpss_output)
+
+kpss_test(train_data['count']) 
+
+# Making Series Stationary
+train_data['count_diff'] = train_data['count'] - train_data['count'].shift(1)
+
+plt.figure(figsize=(12,8))
+
+plt.plot(train_data.index, train_data['count'], label='train_data')
+plt.plot(train_data.index,train_data['count_diff'], label='stationary series')
+plt.legend(loc='best')
+plt.title("Stationary Series")
+plt.show()
+
+
 
